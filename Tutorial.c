c97de1394676a0c8f2a6e4a64b72361731e090eb
Lugmaner <luki.heel@gmail.com>
2025-05-05 19:45:17 +0200

    simple Calculator

--- Tutorial.c
+++ Tutorial.c
@@ -3,6 +3,42 @@
 
 int main()
 {
-    printf("Hello Wordl!\n");
+    char operator;
+    double num1;
+    double num2;
+    double result;
+
+   
+    printf("enter an operator ( + - * /): ");
+    scanf("%c", &operator);
+
+    printf("enter the first number: ");
+    scanf("%lf", &num1);
+
+    printf("enter the second number: ");
+    scanf("%lf", &num2);
+
+    switch(operator){
+        case '+':
+            result = num1 + num2;
+            printf("\nresult: %lf", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("\nresult: %lf", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("\nresult: %lf", result);
+            break;
+        case '/':
+            result = num1 / num2;
+            printf("\nresult: %lf", result);
+            break;
+
+        default:
+        printf("%c is not valide", operator);
+    }
+
     return 0;
 }
